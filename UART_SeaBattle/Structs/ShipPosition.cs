07021744a6a9c6d684b0bd7b1ce6f891e371bab5
Stanislav Naftolski <89383394+Ptuxa@users.noreply.github.com>
2024-12-08 18:52:37 +0300

    feat: add map, projectile, collision, ships (#5)

---
+++ UART_SeaBattle/Structs/ShipPosition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UART_SeaBattle.Structs
+{
+    struct ShipPosition
+    {
+        public XYPosition LeftTop;
+        public XYPosition LeftBottom;
+        public XYPosition RightTop;
+        public XYPosition RightBottom;
+
+        public ShipPosition(XYPosition leftTop, XYPosition leftBottom, XYPosition rightTop, XYPosition rightBottom)
+        {
+            LeftTop = leftTop;
+            LeftBottom = leftBottom;
+            RightTop = rightTop;
+            RightBottom = rightBottom;
+        }
+    }
+}
---
+++ UART_SeaBattle/Structs/XYPosition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UART_SeaBattle.Structs
+{
+    public struct XYPosition
+    {
+        public double X;
+        public double Y;
+
+        public XYPosition(double x, double y) 
+        {
+            X = x; 
+            Y = y;
+        }
+    }
+}
