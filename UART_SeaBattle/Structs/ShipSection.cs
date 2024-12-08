f9258bdc7fe8ae8ecbc2b78643e291fc7c1835fd
Stanislav Naftolski <89383394+Ptuxa@users.noreply.github.com>
2024-12-08 19:51:23 +0300

    feat: add zones destroying ships (#7)

---
+++ UART_SeaBattle/Structs/ShipSection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UART_SeaBattle.Structs
+{
+    public struct ShipSection
+    {
+        public XYPosition Position;
+        public bool IsDestroyed;
+
+        public ShipSection(XYPosition position)
+        {
+            Position = position;
+            IsDestroyed = false;
+        }
+    }
+}
